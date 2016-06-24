9a678485b3efbf395e134ceb6397b1fd9a95ef46
CompulsiveCoder <compulsivecoder@gmail.com>
2016-06-24 20:43:45 +1000

    Fixed scripts to support submodule

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
+echo "Starting build for project"
+echo "Dir: $PWD"
+
+DIR=$PWD
+
+cd mod/gitter-cs/
+sh build.sh
+cd $DIR
+
 xbuild src/gitdb.sln /p:Configuration=Release
--- init.sh
+++ init.sh
@@ -1,8 +1,20 @@
 echo "Initializing project"
 echo "  Dir: $PWD"
 
+git submodule update --init --recursive
+
 DIR=$PWD
 
 cd lib
 sh get-libs.sh
 cd $DIR
+
+cd mod/gitter-cs/
+INIT_FILE="init.sh"
+if [ ! -f "$INIT_FILE" ]; then
+  echo "gitter init file not found: $PWD/$INIT_FILE. Did the submodule fail to initialize?"
+else
+  echo "gitter submodule found"
+  sh init.sh
+  cd $DIR
+fi
