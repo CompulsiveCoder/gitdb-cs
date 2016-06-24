d91f7b4a8d399648df3ca9d75e141c184936b216
CompulsiveCoder <compulsivecoder@gmail.com>
2016-06-24 20:03:51 +1000

    Removed unneeded reference

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -30,4 +30,3 @@ fi
 mono nuget.exe install newtonsoft.json -version 8.0.3
 mono nuget.exe install nunit -version 2.6.4
 mono nuget.exe install nunit.runners -version 2.6.4
-mono nuget.exe install sider -version 0.10.0
