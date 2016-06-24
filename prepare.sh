5162f193df99084edffba74506023f450b5c3aee
CompulsiveCoder <compulsivecoder@gmail.com>
2016-06-24 19:54:31 +1000

    Updated prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,6 +2,6 @@ echo "Preparing damanager project"
 echo "  Dir: $PWD"
 
 sudo apt-get update
-sudo apt-get install -y git wget mono-complete redis-server
+sudo apt-get install -y git wget mono-complete
 
 mozroots --import --sync
