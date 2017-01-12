f31ca32f572224b739928107c146561c28e51ae4
CompulsiveCoder <compulsivecoder@gmail.com>
2017-01-12 11:24:55 +1100

    Tweaked test-from-github.sh

--- test-from-github.sh
+++ test-from-github.sh
@@ -17,12 +17,12 @@ echo "  Branch: $BRANCH"
 DIR=$PWD
 
 # If the .tmp/gitdb directory exists then remove it
-if [ -d ".tmp/gitdb" ]; then
+if [ -d ".tmp/gitdb-cs" ]; then
     rm .tmp/gitdb -rf
 fi
 
 git clone https://github.com/CompulsiveCoder/gitdb-cs.git .tmp/gitdb --branch $BRANCH
-cd .tmp/gitdb && \
+cd .tmp/gitdb-cs && \
 sh init-build-test.sh && \
 cd $DIR && \
-rm .tmp/gitdb -rf
+rm .tmp/gitdb-cs -rf
